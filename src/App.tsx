42bb3b4de4ed820c7d852ecc083c4f2a741e1160
Pongsakorn Thipayanate <166195305+pyyupsk@users.noreply.github.com>
2026-05-26 16:50:18 +0700

    feat: add tailwindcss v4 via vite plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div className="grid min-h-svh place-items-center">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+    </div>
+  );
 }
